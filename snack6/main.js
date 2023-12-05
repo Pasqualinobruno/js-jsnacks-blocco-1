9780ec007b2d0eb7bb9e429929a892700a522d67
Pasqualibruno <pasqualinomolosso@gmail.com>
2023-12-05 14:04:25 +0100

    add variable sum

--- snack6/main.js
+++ snack6/main.js
@@ -11,12 +11,38 @@ if (number4.length > 4 || number4.length < 4) {
 
 } else {
     console.log("il numero è coretto");
-   /*  let sum = 0 */
-   /*  for (let i = 0; i < 4; i++) { */
-   /*      sum = sum + number4; */
+    let sum = 0
+    console.log(sum);
+    for (let i = 0; i < (number4.length-1); i++) {
+        const element = number4[i];
+        console.log(element)
+        sum = sum + element
+        
+    }
+    console.log(sum);
 
-   /*  } */
-   /*  //calcola la somma di tutte le cifre che compongono il numero */
-   /*  console.log(sum); */
+
 }
+
+
+
+    /* for (let i = 0; i < 4; i++) { */
+    /*      */
+
+    /*      */
+
+    /* } */
+    //calcola la somma di tutte le cifre che compongono il numero */
+    
+
+/* if (number4 % 10 == 0) { */
+/*     console.log(number4); */
+/*  */
+/*      */
+/* } else { */
+/*     somma = number4 +  */
+/*  */
+/* } */
+
+
 
